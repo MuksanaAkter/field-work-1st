e537cab31ff0c8dda73bfdb399d671afd6e8c617
MuksanaAkter <muksanaakter3@gmail.com>
2025-01-26 22:07:04 +0600

    Update code to print Fibonacci numbers

--- print_numbers.py
+++ print_numbers.py
@@ -1,2 +1,15 @@
-for i in range(1, 11):
-    print(i, end=" ")
+n = 10
+t1, t2 = 0, 1
+
+print("Fibonacci Series:", end=" ")
+for i in range(1, n + 1):
+    if i == 1:
+        print(t1, end=" ")
+        continue
+    if i == 2:
+        print(t2, end=" ")
+        continue
+    next_term = t1 + t2
+    t1 = t2
+    t2 = next_term
+    print(next_term, end=" ")
